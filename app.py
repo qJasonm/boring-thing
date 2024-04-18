c04f6d89903c2bf366d5b64a72b92290b93295cb
qJasonm <117887620+qJasonm@users.noreply.github.com>
2024-04-17 21:34:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,16 +6,15 @@ app = Flask(__name__)
 
 # temperature conversion function
 def temp_converter(kelvin_temp):
-    return (kelvin_temp - 273.15)*(9/5) + 32
+    return (kelvin_temp - 273.15) * (9/5) + 32
 
-def get_weather_info():
-    city = 'denver'
+def get_weather_info(city):
     api_key = '77ccac5efac3aa383ec4263be801fe61'
     weather_info_url = f'https://api.openweathermap.org/data/2.5/weather?q={city},co,us&appid={api_key}'
-    return (requests.get(weather_info_url)).json()
+    return requests.get(weather_info_url).json()
 
-def create_data():
-    weather_info = get_weather_info()
+def create_data(city):
+    weather_info = get_weather_info(city)
     data = {
         "city": weather_info['name'],
         "weather": weather_info['weather'][0]['main'],
@@ -25,15 +24,22 @@ def create_data():
     }
     return data
 
-data = create_data()
-
 @app.route('/Sloanslake')
-def home():
-    return render_template('home.html', data=data)
+def sloanslake():
+    city = 'wheat ridge'
+    data = create_data(city)
+    return render_template('Sloanslake.html', data=data)
+
+@app.route('/CherryCreek')
+def cherrycreek():
+    city = 'denver'
+    data = create_data(city)
+    return render_template('CherryCreek.html', data=data)
 
 # Scheduler to update data every 5 minutes
 scheduler = BackgroundScheduler()
-scheduler.add_job(func=create_data, trigger="interval", seconds=300)
+scheduler.add_job(func=create_data, args=('edge water',), trigger="interval", seconds=300)
+scheduler.add_job(func=create_data, args=('denver',), trigger="interval", seconds=300)
 scheduler.start()
 
 if __name__ == "__main__":
